9fafa77565a9b9de9b277e18b1f6dfab468b7f78
Mark Clores <maclores@my.cspc.edu.ph>
2025-05-25 16:23:37 +0800

    added more libraries required

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,10 @@
 import sys
 import os
 
-# Add the app directory to the Python path
-path = os.path.dirname(os.path.abspath(__file__))
-if path not in sys.path:
-    sys.path.append(path)
+project_home = '/home/markclores/simple-banking-app-v2'
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
-# Import the app as application
+os.environ['FLASK_APP'] = 'app.py'
+
 from app import app as application
-
-# This allows the application to be run directly
-if __name__ == '__main__':
-    application.run() 
